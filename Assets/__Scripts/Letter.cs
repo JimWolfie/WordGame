8474017c3eefd1a1890069200ca3fc1f542e4a09
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2021-01-28 10:55:52 -0500

    updated level

--- Assets/__Scripts/Letter.cs
+++ Assets/__Scripts/Letter.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Letter : MonoBehaviour
 {
+    [Header("Set in Inspector ")]
+    public float timeDuration = 0.5f;
+    public string easingCurve = Easing.InOut;
+
+
     [Header("Set Dynamically")]
     public TextMesh tMesh;
     public Renderer tRend;
     public bool big=false;
+
+    public List<Vector3> pts=null;
+    public float timeStart=-1;
+
     private char _c;
     private Renderer rend;
 
@@ -33,7 +42,7 @@ public class Letter : MonoBehaviour
         get{ return _c.ToString();}
         set{c = value[0];}
     }
-    public bool visable
+    public bool visible
     {
         get{ return (tRend.enabled);}
         set{tRend.enabled = value;}
@@ -46,7 +55,31 @@ public class Letter : MonoBehaviour
     public Vector3 pos
     {
         set {
+           // transform.position = value;
+           Vector3 mid = (transform.position +value)/2f;
+            float mag =(transform.position-value).magnitude;
+            mid+= Random.insideUnitSphere*mag*0.25f;
+            pts = new List<Vector3>(){transform.position, mid, value};
+            if(timeStart == -1)timeStart = Time.time;
+
+        }
+    }
+    public Vector3 posImmediate
+    {
+        set{
             transform.position = value;
         }
+    }
+    private void Update()
+    {
+        if(timeStart ==-1)return;
+
+        float u =(Time.time - timeStart)/timeDuration;
+        u = Mathf.Clamp01(u);
+        float u1 = Easing.Ease(u, easingCurve);
+        Vector3 v = Utils.Bezier(u1, pts);
+        transform.position = v;
+
+        if(u==1)timeStart =-1;
     }
 }
